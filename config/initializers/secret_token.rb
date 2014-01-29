518e93370555ab8e8609fc65f45ae6d8d7537032
Alex K <unityaudio@icloud.com>
2014-01-29 16:49:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'c39a615a1050312d7d0bf20d988cfb5154ef95ac8bb231d5a81d254249de4b2416fa3fc55e2a90faea7e61b40043a0ae6892d96d135584bfb3f91823dde6255d'
